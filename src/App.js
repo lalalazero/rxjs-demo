8d89304137dab3dd854e9cc1a7b099bf73bb17c6
lalalazero <zzzero520@hotmail.com>
2020-09-10 18:12:04 +0800

    first example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { fromEvent } from "rxjs";
+import { scan, throttleTime, map } from "rxjs/operators";
 
 function App() {
+  useEffect(() => {
+    fromEvent(document, "click")
+      .pipe(
+        throttleTime(1000),
+        map((event) => event.clientX),
+        scan((count, clientX) => count + clientX, 0)
+      )
+      .subscribe((count) => console.log(`clicked ${count} times`));
+  }, []);
   return (
-    <div>app</div>
+    <div>
+      <h2>app</h2>
+    </div>
   );
 }
 
